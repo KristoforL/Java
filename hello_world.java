3d3237909d1197d4419c485e7661cfa5210792fd
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-06-30 10:13:26 -0400

    Hello World

---
+++ hello_world.java
@@ -0,0 +1,13 @@
+// This is the the same first type of program that is done from all programming
+
+// You create a class and run the code that is in the block
+public class hello_world{    
+
+    // This code will run a simple string that is to say hellow world from java
+    // This static void is the usual thing that is used when started a method
+    public static void main(String[] args){
+        System.out.println("Hello World from Java");
+
+    }
+
+}
