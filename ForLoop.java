8da6c3ecd7ac617e9b6426cac260ddb2662633c0
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-07-05 23:42:54 -0400

    Working with for loops after some time.

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+class Fordemo{
+
+    public static void main(String[] args) {
+        int count;
+        
+        //No need to put this in  curly braces and it will work
+        //Things have changed since I have used Java
+        for(count =0; count<5; count = count +1)
+            System.out.println("This is count: " + count);
+        
+        System.out.println("Done!");
+
+    }
+}
