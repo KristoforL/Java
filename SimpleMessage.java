a457b36f4d3260328b420a898d2a26a17c1327ee
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-07-06 00:06:21 -0400

    Simple Message copy from my python

---
+++ SimpleMessage.java
@@ -0,0 +1,19 @@
+class SimpleMessage{
+    public static void main(String[] args) {
+    
+        String message = "This is a simple messag.e";
+
+        System.out.println(message);
+
+        message = "I have changed the variable. Did you notice?";
+
+        System.out.println(message);
+
+        message = "This is making sense to all my programming friends.";
+
+        System.out.println(message);
+    
+        System.out.println(message + " I know it looks like I printed this twice but I did not. I instead just concatenated the message alongside this message.");    
+
+    }
+}
