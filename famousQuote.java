b6ed315064eb761861d0fc6b1d6526b40aaab13a
Jacquawn (Kristofor) Lockhart <Krash.interactive@gmail.com>
2020-07-10 14:14:13 -0400

    Adding strings to strings

---
+++ famousQuote.java
@@ -0,0 +1,10 @@
+class famousQuote {
+    public static void main(String[] args){
+      
+        String Famous_Person = "Winston Church";
+        String message = Famous_Person + " once said this: Never Never Never, give up.";
+
+        System.out.println(message);  
+    }
+    
+}
